730bac3cea33f0bbcddb4aa56ef5275bc41783d8
M-Aniq-Nasir <aniqn3@gmail.com>
2025-09-22 20:36:11 +0500

    M Aniq Nasir

---
+++ M Aniq Nasir/Task 1_print.py
@@ -0,0 +1,16 @@
+name = "M ANIQ NASIR"
+age = 19
+height = 5.8
+is_student = True
+print(name)
+print(age)
+print(height)
+print(is_student)
+name = str(input("Your Name: "))
+age = int(input("Your age: "))
+height = float(input("Your height: "))
+is_student = bool(input("You are a student: "))
+print(name)
+print(age)
+print(height)
+print(is_student)
